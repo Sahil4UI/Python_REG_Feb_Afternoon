ca91939105f88169fe00be419afd5a05a13b19b8
Sahil4UI <54834362+Sahil4UI@users.noreply.github.com>
2020-02-12 11:50:21 +0530

    Add files via upload

---
+++ code-1.py
@@ -0,0 +1,21 @@
+'''a = int(input("Enter First Number :"))
+print(type(a))
+b = int(input("enter second number :"))
+c = a+b
+print(c)
+
+'''
+
+#use # for single line comment
+#use ''' for multiline comment
+
+a = float(input("Enter First Number :"))
+b = float(input("enter second number :"))
+c = a+b
+#print("sum of ",a,"and",b,"is",c)
+#print("sum of {} and {} is {}".format(a,b,c))
+#print(f"sum of {a} and {b} is {c}")-
+print("sum is %f"%c)
+print("sum of %f and %f = %f"%(a,b,c))
+
+
